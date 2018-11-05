5878a10f89712b61ce8ef69cb4d0573cce02437c
Serinkan ASCI <sasci@louisarmand.local>
2018-11-05 10:45:42 +0100

    detail discussions, liste messages, menu helpers

---
+++ administrator/components/com_footregion/views/messages/tmpl/default_body.php
@@ -0,0 +1,36 @@
+<?php
+defined('_JEXEC') or die('Restricted Access');
+
+$listOrder	= $this->escape($this->state->get('list.ordering'));
+$listDirn	= $this->escape($this->state->get('list.direction'));
+
+$saveOrder	= $listOrder == 'ordering';
+if ($saveOrder)
+{
+	$saveOrderingUrl = 'index.php?option=com_footregion&task=messages.saveOrderAjax&tmpl=component';
+	JHtml::_('sortablelist.sortable', 'articleList', 'adminForm', strtolower($listDirn), $saveOrderingUrl);
+}
+?>
+
+<?php foreach($this->items as $i => $item): ?>
+	<tr class="row<?php echo $i % 2; ?>">
+		<td class="center hidden-phone">
+			<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+		</td>
+		<td class="nowrap hidden-phone">
+			<?php echo $item->libelle; ?>
+		</td>
+		<td class="center hidden-phone">
+			<?php echo JHtml::_('jgrid.published', $item->published, $i, 'messages.', true); ?>
+		</td>
+		<td class="center hidden-tablet hidden-phone">
+			<?php echo JHtml::_('date', $item->modified, $this->paramDateFmt); ?>
+		</td>
+		<td class="center hidden-tablet hidden-phone">
+			<?php echo (int) $item->hits; ?>
+		</td>
+		<td class="center hidden-phone">
+			<?php echo (int) $item->id; ?>
+		</td>
+	</tr>
+<?php endforeach; ?>
