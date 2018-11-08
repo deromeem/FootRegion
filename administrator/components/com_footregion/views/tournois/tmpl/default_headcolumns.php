998fd72339d38dce8cde107bbc77fa388bc5f3a8
Quentin RIBAU <quentin.ribau@gmail.com>
2018-11-08 15:48:13 +0100

    Ajout de tournoi

--- administrator/components/com_footregion/views/tournois/tmpl/default_headcolumns.php
+++ administrator/components/com_footregion/views/tournois/tmpl/default_headcolumns.php
@@ -9,16 +9,21 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
         <th width="20" class="hidden-phone">
                 <?php echo JHtml::_('grid.checkall'); ?>
         </th>                   
-        <th width="35%">
-        <th width="1%" class="nowrap center hidden-phone">
-		<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 't.id', $listDirn, $listOrder); ?>
-	</th>
+        <th width="80%">
                 <?php echo JHtml::_('grid.sort', 'COM_FOOTREGION_TOURNOIS_NOM', 't.nom', $listDirn, $listOrder) ?>
         </th>
+
         <th width="1%" style="min-width:55px" class="nowrap center ">
                 <?php echo JHtml::_('grid.sort', 'Publié', 't.published', $listDirn, $listOrder) ?>
         </th>
-		<th width="10%">
+        <th width="1%" style="min-width:120px" class="nowrap center">
+                <?php echo JHtml::_('grid.sort', 'Date', 't.modified', $listDirn, $listOrder) ?>
+        </th>
+		<th width="10%" class="nowrap center">
 			<?php echo JHtml::_('grid.sort', 'JGLOBAL_HITS', 't.hits', $listDirn, $listOrder); ?>
 		</th>
+		<th width="1%" class="nowrap center hidden-phone">
+			<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 't.id', $listDirn, $listOrder); ?>
+		</th>
 	</tr>
+
