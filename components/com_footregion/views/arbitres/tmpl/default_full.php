1d33357bd17d981d65165ad7b629ccd76bc048ab
Allan FORSON <aforson@louisarmand.local>
2018-11-22 17:26:02 +0100

    Ajout du détail/liste arbitre

---
+++ components/com_footregion/views/arbitres/tmpl/default_full.php
@@ -0,0 +1,27 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+?>
+
+<h1>Arbitres de Footregion</h1>
+
+<form action="<?php echo htmlspecialchars(JFactory::getURI()->toString()); ?>" method="post" name="adminForm" id="adminForm">
+
+	<table class="category">
+		<thead>
+			<tr>
+			<th class="title">Email</th>
+		</tr>
+		</thead>
+
+		<tbody>
+			<?php foreach($this->tickets as $i => $item) : ?>
+			<tr>
+				<td><?php echo $item->email ?></td>
+			</tr>			
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+</form>
+
+<?php echo $this->pagination->getListFooter(); ?>
