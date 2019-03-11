8b29f2a620b6edd8f130d165403f750bb870c383
Sébastien LEJEUNE <slejeune@louisarmand.local>
2019-03-11 09:54:43 +0100

    Modification droits clubs / mon club

--- components/com_footregion/views/club/tmpl/default.php
+++ components/com_footregion/views/club/tmpl/default.php
@@ -4,17 +4,18 @@ defined('_JEXEC') or die('Restricted access');
 JHtml::_('behavior.framework'); 				// javascript Joomla object for grid.sort !
 
 $user = JFactory::getUser();               		// gets current user object
-$isAdmin = (in_array('13', $user->groups));		// sets flag when user group is '10' that is 'MRH Administrateur 
+$groupeNumb = substr(implode($user->groups),1,2);
+$isAdmin = (in_array($groupeNumb, $user->groups));		// sets flag when user group is '10' that is 'MRH Administrateur 
 ?>
 
 <?php if (!$isAdmin) : ?>
 	<?php echo JError::raiseWarning( 100, JText::_('COM_FOOTREGION_RESTRICTED_ACCESS') ); ?>
 <?php else : ?>
 
-	<h2><?php echo JText::_('COM_FOOTREGION_CLUB')." "; 
+	<h2><?php echo JText::_('COM_FOOTREGION_CLUB')." ";
 	?>
 		<a href="<?php echo JRoute::_('index.php?option=com_footregion&view=club'); ?>">
-			
+
 		</a>
 	</h2>
 
