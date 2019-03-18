c6d6e16fed55dd03fd9a827dd9731933d63c10f3
Armand SIEYE <asieye@louisarmand.local>
2019-03-18 12:12:00 +0100

    modif armand

--- components/com_footregion/views/signalements/tmpl/default.php
+++ components/com_footregion/views/signalements/tmpl/default.php
@@ -12,7 +12,7 @@ $isArbitre = (in_array('12', $user->groups));
 	<?php echo JError::raiseWarning( 100, JText::_('COM_FOOTREGION_RESTRICTED_ACCESS') ); ?>
 <?php else : ?>
 
-	<h2><?php echo JText::_('COM_FOOTREGION_SIGNALEMENTS')." - "; ?>
+	<h2><?php echo JText::_('COM_FOOTREGION_SIGNALEMENTS'); ?>
 	</h2>
 
 	<?php echo $this->loadTemplate('items'); ?>
