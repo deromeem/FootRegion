e107ca198f66c018441ad9e05f49f89cd95fa38f
Romain DRUART <rdruart@louisarmand.local>
2019-04-15 11:01:17 +0200

    mise à jour des droits de consulation de tournois

--- components/com_footregion/views/tournoi/tmpl/default.php
+++ components/com_footregion/views/tournoi/tmpl/default.php
@@ -1,14 +1,3 @@
-<?php
-defined('_JEXEC') or die('Restricted access');
-
-$user = JFactory::getUser();               		// gets current user object
-$isAdmin = (in_array('9', $user->groups));		// sets flag when user group is '1' that is 'FootRegion Public
-$isArb = (in_array('1', $user->groups));		// sets flag when user group is '10' that is 'MRH Administrateur
-?>
-
-<?php if (!$isAdmin && !$isArb && !$isPub && !$isPub1 && !$isPub2 && !$isPub3 && !$isPub4) : ?>a
-	<?php echo JError::raiseWarning( 100, JText::_('COM_FOOTREGION_RESTRICTED_ACCESS') ); ?>
-<?php else : ?>
 	<div class="form-inline form-inline-header">
 		<div class="btn-group pull-left">
 			<h2><?php echo JText::_('COM_FOOTREGION_TOURNOI'); ?></h2>
@@ -51,4 +40,3 @@ $isArb = (in_array('1', $user->groups));		// sets flag when user group is '10' t
 			</tbody>
 		</table>
 	</div>
-<?php endif; ?>
--- components/com_footregion/views/tournois/tmpl/default.php
+++ components/com_footregion/views/tournois/tmpl/default.php
@@ -1,16 +1,10 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
 
 JHtml::_('behavior.framework'); 				// javascript Joomla object for grid.sort !
 
-$user = JFactory::getUser();               		// gets current user object
-$isAdmin = (in_array('9', $user->groups));		// sets flag when user group is '10' that is 'MRH Administrateur 
-$isAbr = (in_array('1', $user->groups));		// sets flag when user group is '1' that is 'FootRegion Public
 ?>
 
-<?php if (!$isAdmin && !$isAbr && !$isPub && !$isPub1 && !$isPub2 && !$isPub3 && !$isPub4) : ?>
-	<?php echo JError::raiseWarning( 100, JText::_('COM_FOOTREGION_RESTRICTED_ACCESS')); ?>
-<?php else : ?>
+
 
 	<h2><?php echo JText::_('COM_FOOTREGION_TOURNOIS'); ?>
 	</h2>
@@ -19,4 +13,4 @@ $isAbr = (in_array('1', $user->groups));		// sets flag when user group is '1' th
 
 	<?php echo $this->pagination->getListFooter(); ?>
 
-<?php endif; ?>
+
